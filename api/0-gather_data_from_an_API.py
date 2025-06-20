10192da75ee910957883b7700e4e4716b62c26a3
SMunganyinka <s.munganyin@alustudent.com>
2025-06-20 04:39:42 -0700

    made updates

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,28 +1,43 @@
 #!/usr/bin/python3
-""" Library to gather data from an API """
-
 import requests
 import sys
-""" Function to gather data from an API """
 
 if __name__ == "__main__":
-    employee_id = sys.argv[1]
+    if len(sys.argv) != 2:
+        print("Usage: {} EMPLOYEE_ID".format(sys.argv[0]))
+        sys.exit(1)
 
-    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(
-        employee_id)
-    todo_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
-        employee_id)
+    try:
+        employee_id = int(sys.argv[1])
+    except ValueError:
+        print("Employee ID must be an integer")
+        sys.exit(1)
 
-    user_info = requests.request("GET", user_url).json()
-    todo_info = requests.request("GET", todo_url).json()
+    base_url = "https://jsonplaceholder.typicode.com"
 
-    employee_name = user_info.get("name")
-    task_completed = list(
-        filter(lambda x: (x["completed"] is True), todo_info))
-    task_completed_count = len(task_completed)
-    total_tasks = len(todo_info)
+    # Fetch user info
+    user_url = "{}/users/{}".format(base_url, employee_id)
+    user_response = requests.get(user_url)
+    if user_response.status_code != 200:
+        print("Employee not found.")
+        sys.exit(1)
 
-    print("Employee {} is done with tasks({}/{}):".format(employee_name,
-          task_completed_count, total_tasks))
+    user_data = user_response.json()
+    employee_name = user_data.get("name")
 
-    [print("\t {}".format(task.get("title"))) for task in task_completed]
+    # Fetch todos
+    todos_url = "{}/todos?userId={}".format(base_url, employee_id)
+    todos_response = requests.get(todos_url)
+    todos = todos_response.json()
+
+    # Completed tasks
+    done_tasks = [task for task in todos if task.get("completed")]
+    total_tasks = len(todos)
+    done_count = len(done_tasks)
+
+    # Output
+    print("Employee {} is done with tasks({}/{}):".format(employee_name, done_count, total_tasks))
+    for task in done_tasks:
+        print("\t {}".format(task.get("title")))
+
+
