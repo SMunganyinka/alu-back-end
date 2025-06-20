ed2f67152b719842a16ce91d0a374083eb91839e
SMunganyinka <s.munganyin@alustudent.com>
2025-06-20 04:46:47 -0700

    made updates

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
 
     base_url = "https://jsonplaceholder.typicode.com"
 
-    # Fetch user info
+    # Get user information
     user_url = "{}/users/{}".format(base_url, employee_id)
     user_response = requests.get(user_url)
     if user_response.status_code != 200:
@@ -25,19 +25,19 @@ if __name__ == "__main__":
     user_data = user_response.json()
     employee_name = user_data.get("name")
 
-    # Fetch todos
+    # Get TODO list for user
     todos_url = "{}/todos?userId={}".format(base_url, employee_id)
     todos_response = requests.get(todos_url)
     todos = todos_response.json()
 
-    # Completed tasks
-    done_tasks = [task for task in todos if task.get("completed")]
-    total_tasks = len(todos)
-    done_count = len(done_tasks)
+    # Filter completed tasks
+    completed_tasks = [task for task in todos if task.get("completed") is True]
 
-    # Output
-    print("Employee {} is done with tasks({}/{}):".format(employee_name, done_count, total_tasks))
-    for task in done_tasks:
+    # Print required output
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee_name, len(completed_tasks), len(todos)
+    ))
+
+    for task in completed_tasks:
         print("\t {}".format(task.get("title")))
-
 
